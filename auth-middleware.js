100956607eeeb2697cb86186e28ee42e1bada395
Mind Mend Team - KMIT <mindmend2025@gmail.com>
2025-12-10 14:29:18 +0530

    Delete auth-middleware.js

--- auth-middleware.js
+++
@@ -1,4 +0,0 @@
-function checkAuth(){const t=localStorage.getItem("user"),
-  e=window.location.pathname;
-  return!(!t&&"/login.html"!==e)||(window.location.href="/login.html",!1)}
-  "loading"===document.readyState?document.addEventListener("DOMContentLoaded",checkAuth):checkAuth();
